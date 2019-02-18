fe45930f62f35013d44733afee52293632aacd06
Beknazar Suranchiyev <Suranchiyev96@gmail.com>
2019-02-18 14:21:03 -0500

    CORE11

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 //Install express server
 const express = require('express');
-const path = require('path');
+var cors = require('cors');
 
+const path = require('path');
 const app = express();
 
 app.use(cors());
+var cors = require('cors');
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
